c2de13cfd181acfc03b7014b639724eee02d1df6
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2022-09-05 10:52:03 +0800

    rpmsg str echo: move cortex-m4 example to an new dir core_m4
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- evkmimx8mm/multicore_examples/rpmsg_lite_str_echo_rtos/core_m4/armgcc/build_all.sh
+++ evkmimx8mm/multicore_examples/rpmsg_lite_str_echo_rtos/core_m4/armgcc/build_all.sh
@@ -3,41 +3,41 @@ if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug  .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug  .
 make -j
 
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=release  .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=release  .
 make -j
 
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_debug  .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_debug  .
 make -j
 
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release  .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release  .
 make -j
 
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flash_debug  .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flash_debug  .
 make -j
 
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flash_release  .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../core/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flash_release  .
 make -j
 
