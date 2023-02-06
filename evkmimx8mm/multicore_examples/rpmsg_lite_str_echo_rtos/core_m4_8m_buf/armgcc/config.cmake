54ba7f5807f2462978854b4970a4df0b86cef8ef
Chunguang Li <chunguang.li_1@nxp.com>
2023-02-06 18:05:18 +0800

    imx8mmevk: str echo: copy core_m4 as the base of rpmsg 8M buffer support
    Signed-off-by: Chunguang Li <chunguang.li_1@nxp.com>

---
+++ evkmimx8mm/multicore_examples/rpmsg_lite_str_echo_rtos/core_m4_8m_buf/armgcc/config.cmake
@@ -0,0 +1,6 @@
+# config to select component, the format is CONFIG_USE_${component}
+set(CONFIG_USE_middleware_multicore_rpmsg_lite_freertos true)
+set(CONFIG_USE_component_serial_manager_uart true)
+set(CONFIG_USE_driver_iuart true)
+set(CONFIG_USE_middleware_freertos-kernel_MIMX8MM6 true)
+set(MCUX_DEVICE "MIMX8MM6")
