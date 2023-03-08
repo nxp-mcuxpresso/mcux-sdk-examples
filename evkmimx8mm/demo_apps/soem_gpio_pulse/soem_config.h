2abc665f94ac036b6bbf485952a167659671056e
qizhang <jony.zhang@nxp.com>
2023-03-08 22:51:08 +0800

    SOEM: add soem_gpio_pulse example to evkmimx8mm platform
    Set ENET_REF_CLK_ROOT clock.
    
    Signed-off-by: Wenbin Song <wenbin.song@nxp.com>
    Signed-off-by: qizhang <jony.zhang@nxp.com>

---
+++ evkmimx8mm/demo_apps/soem_gpio_pulse/soem_config.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022-2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __SOEM__CONFIG__H__
+#define __SOEM__CONFIG__H__
+#include <sys/time.h>
+
+/** max. etries in EtherCAT error list */
+#define EC_MAXELIST       64
+
+/** max. length of readable name in slavelist and Object Description List */
+#define EC_MAXNAME        40
+
+/** max. number of slaves in array */
+#define EC_MAXSLAVE       100
+
+/** max. number of groups */
+#define EC_MAXGROUP       2
+
+/** max. number of IO segments per group */
+#define EC_MAXIOSEGMENTS  64
+
+/** max. mailbox size */
+#define EC_MAXMBX         1486
+
+/** max. eeprom PDO entries */
+#define EC_MAXEEPDO       0x200
+
+/** max. SM used */
+#define EC_MAXSM          8
+
+/** max. FMMU used */
+#define EC_MAXFMMU        4
+
+/** max. Adapter */
+#define EC_MAXLEN_ADAPTERNAME    128
+
+/* CoE */
+/** max entries in Object Description list */
+#define EC_MAXODLIST   1024
+
+/** max entries in Object Entry list */
+#define EC_MAXOELIST   256
+
+/* OS */
+#define MAX_SOEM_TASK 1
+
+/* Static allocate for SOEM stack when configSUPPORT_STATIC_ALLOCATION is configured*/
+#define MAX_SOEM_TASK_STACK 256
+#define MAX_SOEM_MUTE 1
+
+void osal_gettime(struct timeval *);
+#endif
